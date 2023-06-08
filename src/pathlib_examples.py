5e68668923ea2d57cfb0cda03ae62b90e2f30f70
Pawel Wiszniewski <pawel.wiszniewski@goflink.com>
2023-06-08 08:50:39 +0200

    add pathlib_examples.py

---
+++ src/pathlib_examples.py
@@ -0,0 +1,20 @@
+import json
+import os
+from typing import Any
+
+
+def main():
+    config = read_config("config.json")
+    print(config)
+
+
+def read_config(file_name: str) -> dict[str, Any]:
+    root_folder = os.path.dirname(os.path.abspath(__file__))
+    config_folder = os.path.join(root_folder, "config")
+    with open(f"{config_folder}/{file_name}", "rt") as config_file:
+        config = json.load(config_file)
+    return config
+
+
+if __name__ == "__main__":
+    main()
