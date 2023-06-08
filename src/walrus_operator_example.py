79060525b51ca9f990bc09ad767c2c6eeeb9f66c
Pawel Wiszniewski <pawel.wiszniewski@goflink.com>
2023-06-08 10:15:02 +0200

    add tasks and more code to walrus_operator_example.py

--- src/walrus_operator_example.py
+++ src/walrus_operator_example.py
@@ -1,36 +1,49 @@
-import time
-from typing import Any
+# tasks:
+# 1 - replace all imported typing classes with builtin and Union operator
+# 2 - refactor retrieve_records using walrus := operator
+# 3 - refactor merge_records using union | or |= operator
 
-Record = dict[str, Any]
+import time
+from typing import List, Dict, Optional
 
 
 def main():
     records = retrieve_records()
-    print(records)
-    assert len(records) == 12
+    print(f"Records retrieved: {records}")
+    assert len(records) == 4
+    merged = merge_records(records)
+    print(f"Records merged: {merged}")
+    assert len(merged.keys()) == 4
 
 
-def retrieve_records() -> list[int]:
+def retrieve_records() -> List[Dict[str, str]]:
     param = 0
     all_records = []
 
-    records = send_request(param)
+    record = send_request(param)
 
-    while len(records) > 0:
-        all_records.extend(records)
+    while record is not None:
+        all_records.append(record)
         param += 1
-        records = send_request(param)
+        record = send_request(param)
 
     return all_records
 
 
-def send_request(run_number: int) -> list[int]:
+def merge_records(records: List[Dict[str, str]]) -> Dict[str, str]:
+    merged_record = {}
+    for record in records:
+        merged_record = {**merged_record, **record}
+    return merged_record
+
+
+def send_request(run_number: int) -> Optional[Dict[str, str]]:
     # this could be a long operation
     # time.sleep(1)
     if run_number <= 3:
-        return [value for value in range(3)]
+        return {f"key{run_number}": "value"}
     else:
-        return []
+        return None
 
 
 if __name__ == "__main__":
